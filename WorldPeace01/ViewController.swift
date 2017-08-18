4a1b9a47cf95784e24cd1174b22cd2f76be80815
Sophia <sophshaw99@gmail.com>
2017-08-17 21:22:06 -0700

    initial commit

---
+++ WorldPeace01/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WorldPeace01
+//
+//  Created by Sophia Shaw on 8/17/17.
+//  Copyright © 2017 Soph Shaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print ("Hello")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
